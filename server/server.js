c400f84e84281e6612b08d53b820f88e0091601a
Lounnaci-gi <ahmedlounnaci@gmail.com>
2025-03-04 23:59:15 +0100

    server

--- server/server.js
+++ server/server.js
@@ -6,6 +6,15 @@ const path = require("path");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Servir les fichiers statiques du dossier "client"
+app.use(express.static(path.join(__dirname, '../client')));
+
+// Route par défaut : renvoyer "index.html"
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
+
+
 // Démarrage du serveur
 app.listen(port, () => {
     console.log(`Le serveur est lancé sur le port: http://localhost:${port}`);
